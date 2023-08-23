38c204e5b6f8d2e2e35062f49a3c201450db57bb
athazahrane <elangathazahran@smkwikrama.sch.id>
2023-08-23 15:13:10 +0700

    updateCalculator

--- script.js
+++ script.js
@@ -2,7 +2,12 @@ function insert(num) {
     document.form.textarea.value = document.form.textarea.value + num;
 }
 
+function insert(symbol) {
+    document.form.textarea.value = document.form.textarea.value + symbol;
+}
+
 function equal() {
     var results = document.form.textarea.value;
     document.form.textarea.value = eval(results);
 }
+
